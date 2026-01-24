b86876d602d3c2a276457b2489d108b6f36d27a4
Lukas Schneider <lukas@softcrate.de>
2026-01-24 16:46:29 +0100

    Fix: Strict WWW to NON-WWW redirect middleware

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,16 +1,10 @@
 export async function onRequest(context) {
-    const { request, next } = context;
-    const url = new URL(request.url);
+    const url = new URL(context.request.url);
 
-    // 301 Redirect from WWW to NON-WWW
-    if (url.hostname.startsWith('www.')) {
-        const newHostname = url.hostname.replace('www.', '');
-        const newUrl = new URL(request.url);
-        newUrl.hostname = newHostname;
-
-        return Response.redirect(newUrl.toString(), 301);
+    if (url.hostname === "www.softcrate.de") {
+        url.hostname = "softcrate.de";
+        return Response.redirect(url.toString(), 301);
     }
 
-    // Continue to the next middleware or asset
-    return next();
+    return context.next();
 }
